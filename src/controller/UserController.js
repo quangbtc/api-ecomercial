059b1190c83cf6f3863b1b44edc613c585cf8096
Quang PT <phungthanhquang73@gmail.com>
2023-02-08 16:39:14 +0700

    update

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,38 +1,56 @@
-import User from "../models/User.js"
-import CryptoJS from "crypto-js";
-import { verifyToken } from "../midleware/verifyToken.js";
-export const updateUser= async(req,res,next)=>{
-    if(req.body.password){
-      req.body.password= CryptoJS.AES.encrypt(
-        req.body.password,
-        process.env.PASS_SEC
-      ).toString()
+import User from '../models/User.js';
+import CryptoJS from 'crypto-js';
+import { verifyToken } from '../midleware/verifyToken.js';
+export const updateUser = async (req, res, next) => {
+    if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC,
+        ).toString();
     }
     try {
-        const updateUser = await User.findByIdAndUpdate(req.params.id,{$set:req.body}, {
-          new: true,
-        });
+        const updateUser = await User.findByIdAndUpdate(
+            req.params.id,
+            { $set: req.body },
+            {
+                new: true,
+            },
+        );
         return res.status(200).json(updateUser);
-      } catch (err) {
+    } catch (err) {
         next(err);
-      }
-
-}
-export const deleteUser=async(req,res,next)=>{
-  try {
-        await User.findByIdAndRemove(req.params.id)
-        res.status(200).json('Account was deleted.')     
-  } catch (err) {
-    next(err)
-  }
-
-}
-export const getAllUser=async (req,res,next)=>{
-        try {
-          const users=await User.find().sort({_id:-1})
-          res.status(200).json(users)        
-        } catch (error) {
-          res.status(401).json(error)
+    }
+};
+export const deleteUser = async (req, res, next) => {
+    try {
+        await User.findByIdAndRemove(req.params.id);
+        res.status(200).json('Account was deleted.');
+    } catch (err) {
+        next(err);
+    }
+};
+export const getAllUser = async (req, res) => {
+    try {
+        const users = await User.find().sort({ _id: -1 });
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(401).json(error);
+    }
+};
+export const getUserById = async (req, res) => {
+    const userId = req.params.id;
+    try {
+        const user = await User.findOne({
+            _id: userId,
+        });
+        if (!user) {
+            return res.status(201).json({
+              message:"User not found!"
+            });
         }
-}
-
+        const {password,...others}=user._doc
+        res.status(200).json({ ...others });
+    } catch (error) {
+        res.status(401).json(error);
+    }
+};
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,9 +1,19 @@
-import express from "express";
-import { verifyToken,verifyTokenAndAuthorization,verifyTokenAndAdmin } from "../midleware/verifyToken.js";
-import {updateUser,deleteUser,getAllUser} from "../controller/UserController.js"
+import express from 'express';
+import {
+    verifyToken,
+    verifyTokenAndAuthorization,
+    verifyTokenAndAdmin,
+} from '../midleware/verifyToken.js';
+import {
+    updateUser,
+    deleteUser,
+    getAllUser,
+    getUserById,
+} from '../controller/UserController.js';
 const router = express.Router();
-router.get("/", verifyTokenAndAdmin,getAllUser);
-router.put("/update/:id", verifyTokenAndAuthorization,updateUser);
-router.delete("/delete/:id",verifyTokenAndAuthorization,deleteUser);
+router.get('/find/:id', getUserById);
+router.put('/update/:id', verifyTokenAndAuthorization, updateUser);
+router.delete('/delete/:id', verifyTokenAndAuthorization, deleteUser);
+router.get('/', getAllUser);
 
 export default router;
